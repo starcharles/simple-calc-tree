4ee89a27af3255b0b61488b75ef088b0a83f9e16
Naoto Satoh <stardustforever012@gmail.com>
2017-12-04 02:49:09 +0900

    update readme , update main.rb

--- main.rb
+++ main.rb
@@ -5,21 +5,33 @@ require "pry-byebug"
 require_relative "lib/stree"
 
 # syntax_tree
-# root
-# left1 right1
+#
+#          root - OP_ADD
+#     ______|______
+#    |              |
+# left1 = 2       right1 = OP_SUB
+#                 ___|___
+#                |       |
+#           right11=3  right11=1
 
 root = STree::Node.new
 tree = STree::Tree.new(root)
-
-right1 = STree::Node.new
-left1 = STree::Node.new
-
 root.value = STree::Operations::OP_ADD
-right1.value = 1
+left1 = STree::Node.new
 left1.value = 2
+right1 = STree::Node.new
+right1.value = STree::Operations::OP_SUB
+
+right11 = STree::Node.new
+right11.value = 3
+right12 = STree::Node.new
+right12.value = 1
 
 root.rightNode = right1
 root.leftNode = left1
+
+right1.leftNode = right11
+right1.rightNode = right12
 
 p tree.exec_calculation
 
